4a3aa4a9fdf4c37f697473eea1ea9c46a1085242
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-30 05:30:38 +0100

    Initial Commit

---
+++ OnPressedOnRelease/OnPressedOnReleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnPressedOnReleaseApp.swift
+//  OnPressedOnRelease
+//
+//  Created by Sebastien REMY on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnPressedOnReleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
